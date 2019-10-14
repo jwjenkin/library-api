5e4b9bceb0bedbddf500afd6c371d6ed7ee744f5
Jeremy Jenkins <jeremyjenkins249@gmail.com>
2019-10-14 10:54:09 -0600

    chore(api): fix tabbing from `vim`

--- api/files.js
+++ api/files.js
@@ -9,14 +9,13 @@ const router = require('express')(),
 moment().utcOffset(-5);
 
 router.get('/:id', (req, res) => {
-  File.findById(req.params.id)
-    .then(file => {
-      if ( !file ) {
-        return res.status(404).json({ error: 'File not found.' });
-      } // end if
+  File.findById(req.params.id).then(file => {
+    if ( !file ) {
+      return res.status(404).json({ error: 'File not found.' });
+    } // end if
 
-      res.json(file);
-    }).catch(err => (console.log(err), res.sendStatus(400)));
+    res.json(file);
+  }).catch(err => (console.log(err), res.sendStatus(400)));
 });
 
 router.get('/:id/request', (req, res) => {
@@ -31,21 +30,20 @@ router.get('/:id/stream', (req, res) => {
     return res.status(401).json({ error: 'Missing keycard.' });
   } // end if
 
-  File.findById(req.params.id, { path: 1 })
-    .then(file => {
-      if ( !file ) {
-        return res.status(404).json({ error: 'File not found.' });
+  File.findById(req.params.id, { path: 1 }).then(file => {
+    if ( !file ) {
+      return res.status(404).json({ error: 'File not found.' });
+    } // end if
+    console.log(file.path);
+    res.download(file.path, e => {
+      if ( !e ) {
+        console.log('well, something odd is happening: ');
+      } else if ( e.toString().indexOf('aborted') === -1 ) {
+        console.log(e.toString());
+        //return res.sendStatus(400);
       } // end if
-      console.log(file.path);
-      res.download(file.path, e => {
-	if ( !e ) {
-	  console.log('well, something odd is happening: ');
-	} else if ( e.toString().indexOf('aborted') === -1 ) {
-          console.log(e.toString());
-	  //return res.sendStatus(400);
-	} // end if
-      });
-    }).catch(err => console.log(err));
+    });
+  }).catch(err => console.log(err));
 });
 
 module.exports = router;
--- api/users.js
+++ api/users.js
@@ -37,29 +37,27 @@ router.post('/update-password', (req, res) => {
   } else if ( req.body.newPassword !== req.body.confirmPassword ) {
     error = 'Please make sure password and confirmation match.';
   } // end if
-  
+
   if ( error ) {
     return res.status(400).json({ error });
   } // end if
 
-  User.findById(req.auth._id, req.body, { new: true })
-    .then(user => {
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if ( !valid ) {
-            return res.status(400).json({
-              error: 'Current password incorrect.'
-            });
-          } // end if
-          
-          bcrypt.hash(req.body.newPassword, 10).then(hash => {
-            user.password = hash;
-            user.save();
-            
-            res.json({ success: true });
-          });
-        }).catch(() => res.sendStatus(400));
+  User.findById(req.auth._id, req.body, { new: true }).then(user => {
+    bcrypt.compare(req.body.password, user.password).then(valid => {
+      if ( !valid ) {
+        return res.status(400).json({
+          error: 'Current password incorrect.'
+        });
+      } // end if
+
+      bcrypt.hash(req.body.newPassword, 10).then(hash => {
+        user.password = hash;
+        user.save();
+
+        res.json({ success: true });
+      });
     }).catch(() => res.sendStatus(400));
+  }).catch(() => res.sendStatus(400));
 });
 
 module.exports = router;
