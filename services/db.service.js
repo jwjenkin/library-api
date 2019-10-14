1bfd62e9b7659524b1b40e58b4867faf7a379f9c
jwjenkin <jwjenkin@vault101>
2019-10-14 14:27:05 -0400

    chore(deps): update node dependencies to most up-to-date versions

--- services/db.service.js
+++ services/db.service.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose'),
   pass = process.env.LIBRARY_MONGO_PASS || 'password',
   url = process.env.LIBRARY_MONGO_URL || 'localhost:27017';
 
-mongoose.connect(`mongodb://${user}:${pass}@${url}`, { useMongoClient: true });
+mongoose.connect(`mongodb://${user}:${pass}@${url}`, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
