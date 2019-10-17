04dcbb5ba7f6506b075033abac954a7bad2806d7
jwjenkin <jwjenkin1@gmail.com>
2019-10-17 10:08:48 -0600

    fix(config): add config updates

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
+  deezer: {
+    appId: 375244,
+    redirectUri: 'https://library.jwjenkin.com/deezer',
+    url: 'https://connect.deezer.com'
+  },
   secret: 'y89awfegy8934tasdfl83rag*D)F'
 };
